9439a804fc6d5e9e59ed1d3325332e227bb09eae
Mark Spain <markyspain421@gmail.com>
2014-09-11 11:39:36 -0700

    forgot closing quote

--- tomcat.sh
+++ tomcat.sh
@@ -9,5 +9,5 @@ export CATALINA_HOME=/opt/tomcat
 alias tomcat="cd $CATALINA_HOME"
 
 export SPHOME=$CATALINA_HOME/webapps/identityiq
-alias workspace="cd /home/mspain/Developer/workspace/TVA_IIQ 
+alias workspace="cd /home/mspain/Developer/workspace/TVA_IIQ" 
 
