4529eb7a4f7657159283388ecae4461c4362fd64
Mark Spain <markyspain421@gmail.com>
2014-10-06 18:53:19 -0700

    add functions from older scripts

--- functions.sh
+++ functions.sh
@@ -14,16 +14,22 @@ qfind() {
   return 0
 }
 
-# os-specific ls options
+# os-specific ls options (for use in cdl() and cdll())
 # --------------------------------------
 LS="ls -CFh"
 LSL="ls -CFhl"
+LSA="ls -CFha"
+LSLA="ls -CFhla"
 if [[ $OS == Darwin ]]; then
   LS="ls -CFGh"
   LSL="ls -CFGhl"
+  LSA="ls -CFGha"
+  LSLA="ls -CFGhla"
 elif [[ $OS == Linux ]]; then
   LS="ls -CFh --color=always"
   LSL="ls -CFhl --color=always"
+  LSA="ls -CFha --color=always"
+  LSLA="ls -CFhla --color=always"
 fi
 
 # cdl - change to a directory and ls its contents
@@ -38,3 +44,21 @@ cdll() { cd $1 && eval $LSL; }
 # --------------------------------------
 mkcd() { mkdir -p $1 && cd $1; }
 
+# ff - search current directory for specified pattern
+# --------------------------------------
+ff() { eval $LSLA | grep -i --color=always "$1" }
+
+# psgrep - search for the specified process
+# --------------------------------------
+psgrep() {
+  input=$1
+  out=$(ps aux | grep -i --color=always "$input" | grep -v 'grep')
+  [[ -z "$out" ]] && out="No process named $input found."
+  echo $out
+}
+
+# clearfile - clear the contents of a specified file
+# --------------------------------------
+clearfile() {
+  echo -n > $1
+}
