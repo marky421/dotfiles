7ed3bceeca5f53aef9e31363b691776dcf16a8ae
Mark Spain <markyspain421@gmail.com>
2015-05-21 11:01:36 -0700

    fix submodule install logic

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,6 +11,7 @@ echo "Running install.sh"
 
 # variables
 # --------------------------------------
+OS=$(uname)
 dir=$HOME/.dotfiles        # dotfiles directory
 oldDir=$HOME/.dotfiles_old # old dotfiles backup directory
 
@@ -23,23 +24,25 @@ cd $dir
 # --------------------------------------
 files="aliases.sh bashrc bash_load.sh bash_profile bin env.sh extras.sh functions.sh gitconfig oh-my-zsh private vim zshrc"
 
-# install zsh customization files
+# install zsh, vim
 # --------------------------------------
-install_zsh() {
-  if ! [[ -f /bin/zsh || -f /usr/bin/zsh ]]; then
-    # install zsh using homebrew or apt-get depending on os
-    OS=$(uname)
-    [[ $OS == Darwin ]] && brew install zsh
-    [[ $OS == Linux  ]] && sudo apt-get install zsh
-  else
-    # install submodules
-    git submodule update --init --recursive
-    # set the default shell to zsh if it isn't currently set to zsh
-    [[ ! $(echo $SHELL) == $(which zsh) ]] && chsh -s $(which zsh)
-  fi
-}
+if ! [[ -f /bin/zsh || -f /usr/bin/zsh ]]; then
+  # install zsh using homebrew or apt-get depending on os
+  [[ $OS == Darwin ]] && brew install zsh
+  [[ $OS == Linux  ]] && sudo apt-get install zsh
+fi
 
-install_zsh
+if ! [[ -f /bin/vim || -f /usr/bin/vim ]]; then
+  # install vim using homebrew or apt-get depending on os
+  [[ $OS == Darwin ]] && brew install vim
+  [[ $OS == Linux  ]] && sudo apt-get install vim
+fi
+  
+# install submodules (customization files)
+git submodule update --init --recursive
+
+# set the default shell to zsh if it isn't currently set to zsh
+[[ ! $(echo $SHELL) == $(which zsh) ]] && chsh -s $(which zsh)
 
 # backup old dotfiles and create symlinks to new ones
 # --------------------------------------
@@ -53,4 +56,6 @@ for file in $files; do
   echo "Creating symlink to $file in home directory"
   ln -s $dir/$file $HOME/.$file
 done
+
 echo "...done"
+echo "Please log out and back in for changes to take effect."
