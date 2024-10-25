1f6d7baa2e3880e1f2716304322bd4759621bd70
Mark Spain <markyspain421@gmail.com>
2024-10-25 13:44:51 -0700

    update wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -6,7 +6,7 @@
 -- pull in the wezterm API
 local wezterm = require("wezterm")
 
--- Ttis will hold the configuration
+-- This will hold the configuration
 local config = wezterm.config_builder()
 
 -- change the color scheme
@@ -29,6 +29,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.enable_scroll_bar = true
 config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 8
 -- set startup position
