78ab57af3f032d9fd7cf51b5394e2ebca495f212
Mark Spain <markyspain421@gmail.com>
2025-08-29 16:54:30 -0700

    update apt sources to new format for debian trixie

--- scripts/init-server.sh
+++ scripts/init-server.sh
@@ -10,10 +10,13 @@ sudo apt update
 sudo apt install -y ca-certificates curl gnupg
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "\
+Types: deb
+URIs: https://download.docker.com/linux/debian/
+Suites: "$(. /etc/os-release && echo "$VERSION_CODENAME")"
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.gpg" | \
+sudo tee /etc/apt/sources.list.d/docker.sources > /dev/null
 sudo apt update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $USER
