2071e620d1545db55da0738aa00179407d26080f
Mark Spain <markyspain421@gmail.com>
2026-05-18 11:45:12 -0700

    remove pyenv-virtualenv reference from env.zsh

--- env.zsh
+++ env.zsh
@@ -54,7 +54,6 @@ export EDITOR='vim'
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 [[ -d $PYENV_ROOT/bin ]] && eval "$(pyenv init - zsh)"
-[[ -d $PYENV_ROOT/bin ]] && eval "$(pyenv virtualenv-init -)"
 
 # set misc home dirs, add all to PATH
 export NVM_DIR=$HOME/.nvm
